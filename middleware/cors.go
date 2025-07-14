d7f661c78d522ae17686e3c4484bb2f2e75e6752
Haekalsss <kallsss145@gmail.com>
2025-07-14 21:31:22 +0700

    add cors api frontend

--- middleware/cors.go
+++ middleware/cors.go
@@ -8,7 +8,7 @@ import (
 func SetupCORS(app *fiber.App) {
 	app.Use(cors.New(cors.Config{
 
-		AllowOrigins: "http://localhost:5173",
+		AllowOrigins: "http://localhost:5173, https://transgo.vercel.app/",
 
 		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
 	}))
